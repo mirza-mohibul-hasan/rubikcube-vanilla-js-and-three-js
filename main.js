73caf72cfac61759568f76a8d59df45459dbaae6
mirza-mohibul-hasan <mirzamohibul618@gmail.com>
2024-10-06 16:30:18 +0600

    Add basic cube creation and color functionality

--- main.js
+++ main.js
@@ -23,3 +23,21 @@ function animate() {
 }
 
 animate();
+
+function createCubes() {
+  const geometry = new THREE.BoxGeometry(2, 2, 2);
+
+  const cubesColors = [
+    [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff],
+  ];
+
+  const cubeMaterial = cubesColors[0].map(
+    (color) => new THREE.MeshBasicMaterial({ color })
+  );
+  const cube = new THREE.Mesh(geometry, cubeMaterial);
+
+  cube.position.set(0, 0, 0);
+  scene.add(cube);
+}
+
+createCubes();
